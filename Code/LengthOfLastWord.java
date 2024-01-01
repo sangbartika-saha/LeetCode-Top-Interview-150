6b89d21d449ec59b794ee8ca21467826bdeae300
Sangbartika Saha <122826755+21051334@users.noreply.github.com>
2024-01-01 12:44:30 +0530

    Added new code

--- Code/LengthOfLastWord.java
+++ Code/LengthOfLastWord.java
@@ -1,17 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int count=0;
-      for(int i=s.length()-1;i>=0;i--){
+        int i=s.length()-1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+      for(;i>=0;i--){
           if(s.charAt(i) != ' '){
               count++;
           }
           else if (s.charAt(i) == ' '){
               break;
           }
-          if (s.charAt(s.length()-1) == ' '){
-              ++count;
-              continue;
-          }
+          
       }  
       return count ;
     }
